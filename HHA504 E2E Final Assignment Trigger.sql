7778f9c86bd0ff5f82584b6227f93a06cf6a98e8
Andrew <andrewhuang58@gmail.com>
2021-12-13 19:41:19 -0500

    trigger added

---
+++ HHA504 E2E Final Assignment Trigger.sql
@@ -0,0 +1,18 @@
+show databases;
+use e2e;
+show tables;
+select * from H1N1 limit 10;
+
+delimiter $$
+create trigger h1n1_concern_trigger before insert on H1N1
+for each row
+begin
+if new.h1n1_concern >3 then
+signal sqlstate '45000'
+set message_text = 'H1N1 concern should be a numerical value between 0 and 3. Please try again.';
+end if;
+end; $$
+delimiter ;
+
+insert into H1N1(h1n1_concern) VALUE (4);
+
